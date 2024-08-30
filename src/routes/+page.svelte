18a2823271bf5e4ef6e6d1e454068ce764e7684c
Sam <sammatanz@gmail.com>
2024-08-30 17:44:52 +1200

    341 design update to high contrast theme (#345)
    * Site Theme Implemented
    
    * Completed Site Design Overhaul
    
    * Update Packages
    
    * Update pnpm-lock.yaml
    
    ---------
    
    Signed-off-by: Sam <sammatanz@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,78 +9,67 @@
 <div class="w-3/4 mx-auto my-24 lg:w-5/12">
 	<!-- Intro -->
 	<section class="my-12">
-		<h1 class="font-bold text-7xl">Kia Ora! I'm Sam.</h1>
-		<h2 class="text-xl text-surface-500-400-token">
+		<h1 class="font-bold dark:text-surface-50 text-7xl">
+			Kia Ora! I'm Sam.
+		</h1>
+		<h2 class="text-xl text-surface-700 dark:text-surface-600">
 			Student & Developer based in Wellington.
 		</h2>
 	</section>
 
 	<!-- Study -->
 	<section class="my-12">
-		<h2
-			class="my-2 text-2xl font-medium underline decoration-[3px] decoration-primary-600"
-		>
-			My Study
-		</h2>
-		<p
-			class="text-lg leading-tight tracking-tight text-surface-500-400-token"
-		>
+		<h2 class="section-title decoration-primary-600">My Study</h2>
+		<p class="section-content">
 			I have studied at Victoria University since 2021, originally
-			studying software engineering before pursueing research in AI, for
-			which I am now completing postgraduate research. I am fortunate to
-			study and work with the
+			studying engineering before pursueing research in AI, for which I am
+			now completing postgraduate research. I am fortunate to study and
+			work with the
 			<a
 				href="https://www.wgtn.ac.nz/cdsai"
-				class="font-medium underline decoration-2 text-surface-600-300-token decoration-primary-600"
-				>Center for Data Science and Artificial Intelligence (CDSAI)</a
-			>.
+				class="link decoration-primary-600"
+			>
+				Center for Data Science and Artificial Intelligence (CDSAI)
+			</a>.
 		</p>
 		<dl class="w-11/12 mx-auto list-dl">
-			<div class="my-6">
+			<div class="list-item">
 				<span class="flex-auto">
 					<div>
-						<dt class="-mx-2 font-medium">
+						<dt class="list-item-title">
 							Bachelor of Science - Computer Science
 						</dt>
-						<p
-							class="text-sm font-semibold text-surface-500-400-token"
-						>
-							Feb 2021 - Jan 2024
-						</p>
+						<p class="list-item-subtitle">Feb 2021 - Jan 2024</p>
 					</div>
 
-					<dd
-						class="leading-normal tracking-tight text-surface-500-400-token"
-					>
+					<dd class="list-item-content">
 						My undergraduate studies started with a Bachelor of
 						Engineering, majoring in Software Engineering. After 3
-						years I switched to a Bachelor of Science in Computer
-						Science to graduate and pursue further study in
-						artificial intelligence. My coursework focused on
-						software development, programming, network engineering,
-						statistics, computer graphics, and cybersecurity.
+						years I transferred to instead complete a Bachelor of
+						Science in Computer Science so I could pursue further
+						study in artificial intelligence. My coursework focused
+						on software development, programming, network
+						engineering, statistics, computer graphics, and
+						cybersecurity.
 					</dd>
 				</span>
 			</div>
-			<div class="my-2">
+			<div class="list-item">
 				<span class="flex-auto">
 					<div>
-						<dt class="-mx-2 font-medium">
+						<dt class="list-item-title">
 							Bachelor of Science (Honours) - Artificial
 							Intelligence
 						</dt>
-						<span
-							class="text-sm font-semibold text-surface-500-400-token"
+						<span class="list-item-subtitle"
 							>Feb 2024 - Nov 2024</span
 						>
 					</div>
 
-					<dd
-						class="leading-normal tracking-tight text-surface-500-400-token"
-					>
+					<dd class="list-item-content">
 						My postgraduate study is focused on AI, with courses on
 						machine learning, data science, and AI applications. <a
-							class="font-medium underline decoration-2 text-surface-600-300-token decoration-primary-600"
+							class="link decoration-primary-600"
 							href="https://github.com/sam-mata/evolution-antarctica"
 							>My research project</a
 						>
@@ -89,7 +78,7 @@
 						project is run in collaboration with the
 						<a
 							href="https://www.wgtn.ac.nz/antarctic"
-							class="font-medium underline decoration-2 text-surface-600-300-token decoration-primary-600"
+							class="link decoration-primary-600"
 						>
 							Antarctic Research Center (ARC)
 						</a>.
@@ -101,14 +90,8 @@
 
 	<!-- Gallery -->
 	<section class="my-12">
-		<h2
-			class="my-2 text-2xl font-medium underline decoration-[3px] decoration-secondary-600"
-		>
-			Project Gallery
-		</h2>
-		<p
-			class="text-lg leading-tight tracking-tight text-surface-500-400-token"
-		>
+		<h2 class="section-title decoration-secondary-600">Project Gallery</h2>
+		<p class="section-content">
 			While most of my work is either not public or still in development,
 			here are a few projects you can check out:
 		</p>
@@ -118,7 +101,7 @@
 			<div
 				class="grid w-full gap-4 mx-auto grid-cols- text-token md:grid-cols-2"
 			>
-				<Card
+				<!-- <Card
 					title="Big Data in NZ"
 					coverPhoto={BigData}
 					subtitle="Web Article"
@@ -129,7 +112,7 @@
 					]}
 					link="https://sammata.shinyapps.io/f1-app/"
 					date="August 2024"
-				/>
+				/> -->
 				<Card
 					title="Ice Sheet Modelling"
 					coverPhoto={IceSheetModelling}
@@ -137,7 +120,7 @@
 					descriptionText="Using evolutionary learning techniques to improve long-term forecasting for changes in the Antarctic ice sheets."
 					badges={[
 						{ text: 'Machine Learning', color: 'yellow' },
-						{ text: 'Data Visualisation', color: 'green' },
+						{ text: 'Research', color: 'purple' },
 					]}
 					link="https://github.com/sam-mata/evolution-antarctica"
 					date="2024"
@@ -168,51 +151,50 @@
 				/>
 			</div>
 		</div>
+		<p class="section-content">
+			Currently I'm focused on continuing my research, as well as making a tool for
+			examining data collection in New Zealand. For more projects, check
+			out
+			<a class="link decoration-secondary-600" href="/projects"
+				>my GitHub portfolio</a
+			>.
+		</p>
 	</section>
 
 	<!-- Work -->
 	<section class="my-12">
-		<h2
-			class="my-2 text-2xl font-medium underline decoration-[3px] decoration-tertiary-600"
-		>
-			My Work
-		</h2>
-		<p
-			class="text-lg leading-tight tracking-tight text-surface-500-400-token"
-		>
+		<h2 class="section-title decoration-tertiary-600">My Work</h2>
+		<p class="section-content">
 			I have worked in a variety of technical roles:
 		</p>
 		<dl class="w-11/12 mx-auto list-dl">
-			<div class="my-6">
+			<div class="list-item">
 				<span class="flex-auto">
 					<div>
-						<dt class="-mx-2 font-medium">
+						<dt class="list-item-title">
 							Academic Tutor - Victoria University of Wellington
 						</dt>
-						<span
-							class="text-sm font-semibold text-surface-500-400-token"
+						<span class="list-item-subtitle"
 							>Feb 2024 - Present</span
 						>
 					</div>
 
-					<dd
-						class="leading-normal tracking-tight text-surface-500-400-token"
-					>
+					<dd class="list-item-content">
 						I have tutored three courses at Victoria University:
 						Programming for the Natural and Social Sciences
 						<a
-							class="font-medium underline decoration-2 text-surface-600-300-token decoration-tertiary-600"
+							class="link decoration-tertiary-600"
 							href="https://www.wgtn.ac.nz/courses/comp/132/2021/offering?crn=30095"
 							>(COMP-132)</a
 						>, Algorithms and Data Structures
 						<a
-							class="font-medium underline text-surface-600-300-token decoration-2 decoration-tertiary-600"
+							class="link decoration-tertiary-600"
 							href="https://www.wgtn.ac.nz/courses/comp/261/2024/offering?crn=18314"
 							>(COMP-261)</a
 						>
 						and Safety Critical Systems
 						<a
-							class="font-medium underline text-surface-600-300-token decoration-2 decoration-tertiary-600"
+							class="link decoration-tertiary-600"
 							href="https://www.wgtn.ac.nz/courses/swen/326/2022/offering?crn=30042"
 							>(SWEN-326)</a
 						>. Each of these involve running tutorials, labs, and
@@ -221,21 +203,18 @@
 					</dd>
 				</span>
 			</div>
-			<div class="my-2">
+			<div class="list-item">
 				<span class="flex-auto">
 					<div>
-						<dt class="-mx-2 font-medium">
+						<dt class="list-item-title">
 							Full Stack Web Developer - Ordino
 						</dt>
-						<span
-							class="text-sm font-semibold text-surface-500-400-token"
+						<span class="list-item-subtitle"
 							>Feb 2022 - Present</span
 						>
 					</div>
 
-					<dd
-						class="leading-normal tracking-tight text-surface-500-400-token"
-					>
+					<dd class="list-item-content">
 						In this role I plan, design, and implement web and
 						mobile interfaces for a progress tracking system for
 						dental technicians. Primarily this involves using React,
@@ -249,16 +228,14 @@
 	<!-- Contact -->
 	<section class="mt-12 -mb-8">
 		<h2
-			class="my-2 text-2xl font-medium underline decoration-[3px] decoration-surface-500-400-token"
+			class="section-title decoration-surface-500 dark:decoration-surface-400"
 		>
 			Contact
 		</h2>
-		<p
-			class="text-lg leading-tight tracking-tight text-surface-500-400-token"
-		>
+		<p class="section-content">
 			While I tend to keep busy with study and work, If you have a project
 			or opportunity you'd like to discuss, please get in touch at <a
-				class="font-medium underline text-surface-600-300-token decoration-2 decoration-surface-500-400-token"
+				class="link decoration-surface-500 dark:decoration-surface-400"
 				href="mailto:sammatanz@gmail.com">sammatanz@gmail.com</a
 			>, or through one of the platforms below.
 		</p>
